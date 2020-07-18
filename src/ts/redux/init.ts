53dac4bec82da31de30753881969676334cb0ab0
Tyler Waters <tyler.waters@gmail.com>
2020-07-17 21:46:11 -0700

    Remind myself why this file is here

--- src/ts/redux/init.ts
+++ src/ts/redux/init.ts
@@ -1,6 +1,8 @@
 import { ScoringType } from './game-state'
 import { Card } from '../lib/Card'
 
+// this is in it's own file because circular dependencies are jerks.
+
 export const INITIALIZE = '@@game-state/initialize'
 export type InitializeAction = { type: typeof INITIALIZE; scoringType: ScoringType; cards: Card[] }
 export const initialize = (scoringType: ScoringType, cards: Card[]): InitializeAction => ({
